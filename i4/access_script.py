f930f55de2ec07ef3b45fb03f2b66439b59611d8
Chance Landis <maolandis@gmail.com>
2025-04-24 15:19:44 -0700

    started working on the assignment

---
+++ i4/access_script.py
@@ -0,0 +1,18 @@
+'''
+Find 3 different information structures with different access technologies
+(e.g. API, DB connection, file download and read locally). Write python
+functions to access each one and print out a sample of the information. In 
+comments for each function, describe pros and cons of the access 
+methodology. Upload code to Github and share URL, make sure any instructions
+needed to run are available in comments, preferably downloading and running
+the python is as simple as possible. 
+'''
+
+# Access Method One
+# CSV download FROM url
+
+# Method 2
+# SQL Endpoint?
+
+# 3
+# manual file read??
